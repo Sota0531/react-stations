a4886b56e32a17c36c2fb52a46cb853f2b5b5cdf
Sota0531 <na.3105.so@icloud.com>
2024-07-08 13:02:55 +0900

    S4.fin

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 // DO NOT DELETE
 
 import './App.css'
+import { useState } from 'react';
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setdogUrl] = useState('https://images.dog.ceo/breeds/entlebucher/n02108000_2064.jpg');
+  
   return (
     <div>
       <header>
         <h1 id = "title"> dog app</h1>
       </header>
       <body>
-        <h2 id = "siteDescription">犬の画像が見れます</h2>
-        <img src = 'https://images.dog.ceo/breeds/entlebucher/n02108000_2064.jpg'></img>
+        <h2 id = "siteDescription">犬の画像が見れます</h2>    
+        
+        <img src = {dogUrl}></img>
+        
       </body>
       
     </div>
